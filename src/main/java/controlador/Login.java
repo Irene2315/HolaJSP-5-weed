fde97259f5bdcdf2eea115df052e9f845b22dede
admin <admin@DESKTOP-UJGJBG5>
2023-03-14 17:17:26 +0100

    subir

---
+++ src/main/java/controlador/Login.java
@@ -0,0 +1,51 @@
+package controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RecogerDatos
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String usuario= request.getParameter("usuario");
+		String password= request.getParameter("password");
+		
+		if(usuario.equals("ikaslea") && password.equals("1234")) {
+			request.getRequestDispatcher("VersionOk.jsp").forward(request, response);
+			
+		}
+		else{
+			request.getRequestDispatcher("VersionFail.jsp").forward(request, response);
+		}
+	}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
